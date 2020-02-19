6809f2811fe82cd8fceb65af9388d461aff845cc
Luis Mendes <cvlopes88@gmail.com>
2020-02-19 15:43:49 -0500

    recursive sorting

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,16 +3,56 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
+    # creating placeholders for where we are in each arr
+    a_index = 0
+    b_index = 0
+
+    # Loop through the length of  the combined elements
+    for k in range(elements):
+        # if one arrA or arr B is empty merge the other list
+        if a_index >= len(arrA):
+            merged_arr[k] = arrB[b_index]
+            b_index += 1
+        elif b_index >= len(arrB):
+            merged_arr[k] = arrA[a_index]
+            a_index += 1
+        # if A[1] is smaller than B[1]
+        elif arrA[a_index] < arrB[b_index]:
+            # append A[1] to the merged array
+            merged_arr[k] = arrA[a_index]
+            a_index += 1
+        # else
+        else:
+            # append B[1] to the end of the merged array
+            merged_arr[k] = arrB[b_index]
+            b_index += 1
+
+    print(merged_arr)
     
     return merged_arr
 
 
+    # merge(arr1, arr2)
+
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-
-    return arr
-
+     # split the list into sublists until they are all length one or less
+       # CEHCK IF LENGTH IS ONE OR LESS IF SO RETURN THE LIST
+       if len(arr) > 1:
+        
+        # divide in half
+        # call split function on left
+        left = arr[:len(arr) // 2]
+        # call split function on right
+        right = arr[:len(arr) // 2:]
+        # Sort the left
+        left = merge_sort(left)
+        # sort the right
+        right = merge_sort(right)
+        arr = merge(left, right)
+       return arr
+       print(merge_sort([3, 4, 1, 5, 3]))
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
@@ -31,3 +71,4 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
